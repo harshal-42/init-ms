8a2615af4e0d976f326b161102d1417effffa0a7
harshal-42 <ext-harshal.pandit@lahitapiola.fi>
2025-01-02 12:07:08 +0530

    Update fetch-config.sh

--- fetch-config.sh
+++ fetch-config.sh
@@ -2,7 +2,7 @@
 set -e 
 echo "Fetching configuration from Azure App Configuration..."
 #variables 
-APP_CONFIG_CONNECTION_STRING="Endpoint=https://appconfig-init-poc.azconfig.io;Id=luRf;Secret=BvlPXVdpk0MhWCRNJAQjWlHYB8pEwJjZCmQiNx0wAhHTsB07IPIxJQQJ99ALAC5RqLJ9DKtpAAACAZAC2FqM"
+APP_CONFIG_CONNECTION_STRING="Endpoint=https://appconfig-cis-cel-sit-02.azconfig.io;Id=KAZJ;Secret=8Mjo7PKZxOleUqdC4EpG9xyzebgQ77FtNmvh9OIsV7DQTEizqcuLJQQJ99BAAC5RqLJ9DKtpAAACAZACIuKp"
 # Fetch key-value pairs from App Configuration
 az appconfig kv list --connection-string "$APP_CONFIG_CONNECTION_STRING" --query '[].{key:key, value:value}' -o json > /shared/config.json
 echo "Configuration saved to /shared/config.json"
